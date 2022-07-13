6c3db67ac58196b4d61a252004a94c9d8ce73516
callmebaby <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2022-07-14 05:07:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,22 +12,21 @@ Just Add me to Your Group As Admin, Hit The Help Button For More Info..</b>"""
 <b>Here Is The Help For My Commands.</b>"""
 
     ABOUT_TXT = """<b>➥ My name: {}
-➥ Creator: <a href='https://t.me/zautekm'>Zaute Km</a>
+➥ Channel: <a href='https://t.me/BAGURUJOINAGUUKANNADAMOVIES_17'>CHANNEL</a>
 ➥ Library: <a href='https://docs.pyrogram.org/'>Pyrogram</a>
 ➥ Language: Python 𝟹
 ➥ Data Base: <a href='https://www.mongodb.com/'>MongoDB</a>
 ➥ Bot Server: <a href='https://heroku.com'>Heroku</a>
 ➥ Build Status: v2.0.1 [ Beta ]"""
 
-    SOURCE_TXT = """<b>Source:</b>
-IMDb is a Open source project.
-Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 👈</a>
+    SOURCE_TXT = """<b>Source:</b>.
+Source: <a href='https://t.me/BAGURUJOINAGUUKANNADAMOVIES_17'>CHANNEL - Click here 👈</a>
 
 <b>DEVS:</b>
 - <a href='https://t.me/josprojects'>Jos Projects</a>
 
 <b>SUPPORT GROUP</b>
-- <a href='https://t.me/+y53tWFUw6Q43NzE9'>Jos Movie Club</a>"""
+- <a href='https://t.me/BAGURUJOINAGUUKANNADAMOVIES_17'>Kannada Movies</a>"""
 
     MANUALFILTER_TXT = """Help: <b>Filters</b>
 
@@ -54,7 +53,7 @@ Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 
 3. Buttons should be properly parsed as markdown format.
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/josprojects)</code>
+<code>[Button Text](buttonurl:https://t.me/BAGURUJOINAGUUKANNADAMOVIES_17)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -279,7 +278,7 @@ Some URLs is Shortner
 • /short <code>(link)</code> - I will send the shorted links.
 
 <b>Example:</b>
-<code>/short https://t.me/josprojects</code>
+<code>/short https://t.me/BAGURUJOINAGUUKANNADAMOVIES_17</code>
 
 <b>NOTE:</b>
 • IMDb should have admin privillage.
